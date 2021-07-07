0321f5c98fbd2fe454175b51cf759c35aa19633a
Nicolò Pomini <nicolo.pomini@u-hopper.com>
2021-07-07 13:47:04 +0200

    added debug prints

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ telegram_api_token = "1842211613:AAHlR-D03I-WmTvku5FRSJqXasHxr6NQYYM"
 
 @app.route(f"/{telegram_api_token}", methods=["POST"])
 def bot_webhook():
+    print("Reuqest received")
     payload = request.json
+    print(payload)
     message = payload["message"]
+    print(message)
     chat_id = message["chat"]["id"]
+    print(chat_id)
     text = message.get("text", "")
+    print(text)
     requests.post(f"https://api.telegram.org/bot{telegram_api_token}/sendMessage", json={
         "chat_id": chat_id,
         "text": "Hello!"
     })
-    return {}
+    return {}, 200
 
 
 if __name__ == "__main__":
