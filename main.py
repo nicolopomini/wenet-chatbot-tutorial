f51db9bff3205cc26339611ebf30620dbc34d55b
Nicolò Pomini <nicolo.pomini@u-hopper.com>
2021-07-08 11:09:32 +0200

    Added function to send a task, and created a callback for wenet

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 import requests
 from flask import Flask, redirect, request
 from wenet.interface.client import Oauth2Client
+from wenet.interface.service_api import ServiceApiInterface
 from wenet.interface.wenet import WeNet
+from wenet.model.task.task import Task, TaskGoal
 from wenet.storage.cache import InMemoryCache
 
 app = Flask(__name__)
 TELEGRAM_API_TOKEN = "1842211613:AAHlR-D03I-WmTvku5FRSJqXasHxr6NQYYM"
 WENET_CLIENT_ID = "A4glP1Fbc6"
+TASK_TYPE_ID = "60e6ba986fba661ff95b5b6a"
 
 cache = InMemoryCache()
-authenticated_users = set()
+authenticated_users = {}
+wenet_id_to_telegram_id = {}
+
+
+def send_task(text: str, telegram_id: int):
+    task = Task(
+        None,
+        None,
+        None,
+        TASK_TYPE_ID,
+        authenticated_users[telegram_id],
+        WENET_CLIENT_ID,
+        None,
+        TaskGoal(text, "")
+    )
+    client = Oauth2Client(
+        client_id=WENET_CLIENT_ID,
+        client_secret="lRlxyuhUAEFxIaVjccpZ",
+        resource_id=str(telegram_id),
+        cache=cache,
+        token_endpoint_url="https://wenet.u-hopper.com/dev/api/oauth2/token"
+    )
+    service_api = ServiceApiInterface(client, "https://wenet.u-hopper.com/dev/api/service")
+    service_api.create_task(task)
 
 
 @app.route(f"/{TELEGRAM_API_TOKEN}", methods=["POST"])
@@ -28,6 +54,19 @@ def bot_webhook():
         requests.post(f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage", json={
             "chat_id": chat_id,
             "text": answer
+        })
+        return {}, 200
+    if text.startswith("/task"):
+        task_text = text[5:]
+        try:
+            send_task(task_text, chat_id)
+            response_text = "Your task has been sent"
+        except Exception as e:
+            print(e)
+            response_text = "Something went wrong in sending the task"
+        requests.post(f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage", json={
+            "chat_id": chat_id,
+            "text": response_text
         })
         return {}, 200
     answer = "Hello!"
@@ -53,15 +92,28 @@ def login():
                                                token_endpoint_url="https://wenet.u-hopper.com/dev/api/oauth2/token",
                                                redirect_url="https://wenet-chatbot-tutorial.herokuapp.com/login"
                                                )
-    authenticated_users.add(int(external_id))
     connector = WeNet.build(client, platform_url="https://wenet.u-hopper.com/dev")
     token_details = connector.service_api.get_token_details()
-    username = connector.service_api.get_user_profile(token_details.profile_id).name.first
+    user_profile = connector.service_api.get_user_profile(token_details.profile_id)
+    username = user_profile.name.first
+    wenet_id = user_profile.profile_id
+    wenet_id_to_telegram_id[wenet_id] = external_id
+    authenticated_users[int(external_id)] = wenet_id
+    print("Saved ID binding")
+    print(wenet_id_to_telegram_id)
     requests.post(f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage", json={
         "chat_id": external_id,
         "text": f"Welcome {username}!"
     })
     return redirect(f"http://wenet.u-hopper.com/dev/hub/frontend/oauth/complete?app_id={WENET_CLIENT_ID}")
+
+
+@app.route("/wenet_callback", methods=["POST"])
+def wenet_callback():
+    print("Callback called")
+    payload = request.json
+    print(payload)
+    return {}, 200
 
 
 if __name__ == "__main__":
